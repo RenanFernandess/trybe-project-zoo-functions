a6c9ce5ac9d4a7486e26ec5084d3fb51e0a708ba
Renan Fernandes <renzinestuods@gmail.com>
2022-06-29 20:23:52 -0300

    Requisito-5: Implementa a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const { species } = data;
+  if (!animal) {
+    return species.reduce((obj, { name, residents }) => {
+      const object = obj;
+      object[`${name}`] = residents.length;
+      return obj;
+    }, {});
+  }
+  const { specie, sex } = animal;
+  return species.find(({ name }) => name === specie).residents
+    .filter(({ sex: animalSex }) => !sex || sex === animalSex).length;
 }
 
 module.exports = countAnimals;
