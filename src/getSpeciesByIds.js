6c1ed3a6936be65ee261c1a849fbc24c88556986
Renan Fernandes <renzinestuods@gmail.com>
2022-06-29 15:32:06 -0300

    Requisito-1: Implementa a função getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  return species.filter(({ id }) => ids.some((value) => value === id));
 }
 
 module.exports = getSpeciesByIds;
