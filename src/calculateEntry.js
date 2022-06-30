c3fded6e94929cb03d506764feb094a84de87dab
Renan Fernandes <renzinestuods@gmail.com>
2022-06-30 15:45:11 -0300

    Requisito-8: a função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter(({ age }) => age < 18).length;
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const senior = entrants.filter(({ age }) => age >= 50).length;
+
+  return { adult, child, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !Object.keys(entrants).length) return 0;
+  const { prices: { child, adult, senior } } = data;
+  const { adult: adultN, child: childN, senior: seniorN } = countEntrants(entrants);
+  return (child * childN) + (adult * adultN) + (senior * seniorN);
 }
 
 module.exports = { calculateEntry, countEntrants };
